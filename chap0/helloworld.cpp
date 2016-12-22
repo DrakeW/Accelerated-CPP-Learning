4e9b34b0f56b1a3ace4166048a2e84362ef13d10
DrakeW <junyuw@berkeley.edu>
2016-12-22 16:15:49 +0800

    chap0&1

---
+++ chap0/helloworld.cpp
@@ -0,0 +1,15 @@
+// a small c++ program
+#include <iostream>
+
+int main()
+{
+	// exercises
+	// 0-0
+	std::cout << "Hello, world!" << std::endl;
+	// 0-2
+	std::cout << "This (\") is a quote, and this (\\) is a backslash." << std::endl;
+	// 0-3
+	std::cout << "tab test\ttab" << std::endl;
+
+	return 0;
+}
